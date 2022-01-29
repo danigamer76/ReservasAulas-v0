141f678b37d1d0da6ef519cafa4a57565662d94b
Daniel <Daniel@DESKTOP-FQNQR1B>
2022-01-29 16:02:21 +0100

    13 - Creada Clase Consola

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -10,20 +10,25 @@ import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Permanenci
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Profesor;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.dominio.Tramo;
 import org.iesalandalus.programacion.reservasaulas.mvc.modelo.negocio.Aulas;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.Consola;
 
 public class MainApp {
 
 	public static void main(String[] args) throws OperationNotSupportedException {
 		System.out.println("Programa para la gestión de reservas de espacios del IES Al-Ándalus");
-		String NOMBRE_SALON1 = "Salón 1";
-		String NOMBRE_SALON2 = "Salón 2";
-		Aula aula1 = new Aula(NOMBRE_SALON1);
-		Aula aula2 = new Aula(NOMBRE_SALON2);
-		Aulas aulas = new Aulas(5);
-		aulas.insertar(aula1);
+//		String NOMBRE_SALON1 = "Salón 1";
+//		String NOMBRE_SALON2 = "Salón 2";
+//		Aula aula1 = new Aula(NOMBRE_SALON1);
+//		Aula aula2 = new Aula(NOMBRE_SALON2);
+//		Aulas aulas = new Aulas(5);
+//		aulas.insertar(aula1);
 //		aulas.borrar(aula1);
-		System.out.println(aulas.getTamano());
-		System.out.println(aulas.buscar(aula1));
+//		System.out.println(aulas.getTamano());
+//		System.out.println(aulas.buscar(aula1));
+
+//		Consola.mostraMenu();
+
+		System.out.println("Prueba 1 \n Prueba 2");
 
 	}
 
